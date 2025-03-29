a7caa9f770bbf140fa88e0ee5d0f43215cba8673
Pamela Peralta <pamperalta@gmail.com>
2025-03-29 14:07:18 -0300

    add `404` and a `500` error handlers

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
+const logger = require("morgan");
+
+app.use(logger("dev"));
 
 const productsRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
 app.use("/products", productsRoutes);
 app.use("/orders", ordersRoutes);
+
+// error handling in case none of the routers above worked
+app.use((req, res, next) => {
+  const error = new Error("Not found :(");
+  error.status = 404;
+  next(error);
+});
+
+// error handling for failing operations
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
